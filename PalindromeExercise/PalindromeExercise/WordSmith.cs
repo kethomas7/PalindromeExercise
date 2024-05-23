28e40c0edbed9a48cb884a3c2ed1f81e9f3392db
Kaitlyn Thomas <kaitlynthomas2416@gmail.com>
2024-05-23 14:35:13 -0500

    completed exercise 2

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+
+        public bool IsAPalindrome(string word)
+        {
+            return word.SequenceEqual(word.Reverse());
+
+            var charArray = word.ToCharArray();
+            Array.Reverse(charArray);
+
+            return word.SequenceEqual(charArray);
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,9 +6,22 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar",true)]
+        [InlineData("house",false)]
+        [InlineData("wow",true)]
+        public void PalindromeTest(string word, bool expected)
         {
+
+            //Arrange
+            var wordSmithObject = new WordSmith();
+
+            //Act
+            var actual = wordSmithObject.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
 
         }
     }
